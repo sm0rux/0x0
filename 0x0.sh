4a6dc14b8fc1ae857d3b26bdfba4bcf4245ab79d
Pontus Falk <pfalk@ax25.org>
2020-07-13 01:40:14 +0200

    Version 0.5

--- 0x0.sh
+++ 0x0.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# 0x0 version 0.4-5
+set -e
+
+# 0x0 version 0.5-1
 # Copyright (C) 2020 Pontus Falk
 
 # Put 0x0.sh in /usr/local/bin directory or make a symbolic link in
@@ -26,97 +28,81 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-echo "0x0 version 0.4-5. Copyright (C) 2020 by Pontus Falk"
-echo "License: MIT license"
-echo
-
 VAR1=$1
 VAR2=$2
 
-# If VAR1 is --help, -h or -? then show these lines and then quit.
-case $VAR1 in
-	"--help"|"-h"|"-?")
-		echo "Format: '$0 [option] [directory/]filename.ext'."
-		echo
-		echo "Option -d: default setting"
-		echo "       -#: show progress-bar"
-		echo "       -s: silent mode (supress errors)"
-		echo "       -S: silten mode (show errors)"
-		exit 1
-esac
+echo "0x0 version 0.5-1. Copyright (C) 2020 by Pontus Falk"
+echo "License: MIT license"
+echo
 
-# If VAR2 is empty and VAR1 is an existing and readable file and if ~/.0x0rc
-# exist then move VAR1 to VAR2 and put content in ~/.0x0rc in VAR1.
-if [ -z $VAR2 ]; then
-	if [ -f ~/.0x0rc ]; then
-		if [ $VAR1 ]; then
-			FILE=~/.0x0rc
-			while read LINE; do
-				# If line in ~/.0x0rc is not a comment and VAR1 is a file then
-				# rotate VAR1->VAR2 and LINE->VAR1
-				if [ ${LINE::1} != "#" ]; then
-					VAR2=$VAR1
-					VAR1=$LINE
-				fi
-			done < $FILE
-		fi
-	fi
-fi
-
-# If VAR1 is not empty and VAR2 is an existing and readable file the do the work :)
-if [ $VAR1 ]; then
-	if [ -f "$VAR2" ]; then
-
-		echo "Uploading '$VAR2'!"
-
-		case $VAR1 in
-			"-d")
-				echo
-				URL=$(curl -F file=@"$VAR2" -- https://0x0.st)
-				;;
- 			"-#")
-				echo
-				URL=$(curl -# -F file=@"$VAR2" -- https://0x0.st)
-				;;
-			"-s")
-				URL=$(curl -s -F file=@"$VAR2" -- https://0x0.st)
-				;;
-			"-S")
-				URL=$(curl -s -S -F file=@"$VAR2" -- https://0x0.st)
-				;;
-			*)
-				echo
-				echo "*** Unsupported option '$VAR1', continue with default setting!"
-				echo
-				URL=$(curl -F file=@"$VAR2" -- https://0x0.st)
-		esac
-
-		echo
-		echo -n $URL | xclip -i -sel clipboard
-		echo "Tjoho, uploading of '$VAR2' is done! The URL is '$URL'!"
-
-	# If VAR1 is an existing and readable file (as it only could be if ~/.0x0rc not exists and no option is given...
-	elif [ -f "$VAR1" ]; then
-		echo "Uploading '$VAR1'!"
-		echo
-		URL=$(curl -F file=@"$VAR1" -- https://0x0.st)
-		echo
-		echo -n $URL | xclip -i -sel clipboard
-		echo "Tjoho, uploading of '$VAR1' is done! The URL is '$URL'!"
-
-	# If no valid file is found in either VAR1 or VAR2
-	else
-		if [ -z $VAR2 ]; then
-			echo "Didn't find the file '$VAR1'!"
-		else
-			echo "Didn't find the file '$VAR2'!"
-		fi
-	fi
-
-# If no option or filename is given...
-else
+# If no options are given, echo shorter help text and exit
+if [ -z $VAR1 ]; then
 	echo "Ehh... you have to give a file name!"
 	echo
 	echo "Correct format is '$0 [option] [directory/]filename.ext'."
 	echo "See $0 --help for more info."
+	exit 1
 fi
+
+# If VAR1 is --help, -h or -?, echo short help text and exit
+if [ "$VAR1" = "--help" ] || [ "$VAR1" = "-help" ] || [ "$VAR1" = "-h" ] || [ "$VAR1" = "-?" ]; then
+	echo "Format: '$0 [option] [directory/]filename.ext'."
+	echo
+	echo "Option -d: default setting"
+	echo "       -#: show progress-bar"
+	echo "       -s: silent mode (supress errors)"
+	echo "       -S: silten mode (show errors)"
+	exit 1
+fi
+
+# Exit if no valid filename is given either in $VAR1 or $VAR2
+if ! [ -f $VAR1 ] && ! [ -f $VAR2 ]; then
+	if [ -z $VAR2 ]; then
+		echo "Didn't find the file '$VAR1'!"
+	else
+		echo "Didn't find the file '$VAR2'!"
+	fi
+	exit 1
+fi
+
+# Read the ~/.0x0rc configuration file
+if [ -f ~/.0x0rc ]; then
+	source ~/.0x0rc
+fi
+
+# If file to transfer is in first option then move it to VAR2 and put CONFIG1 in VAR1
+if [ -f $VAR1 ]; then
+	VAR2=$VAR1
+
+	# If CONFIG1 is empty (no alternative given in ~/.0x0rc then put -d in VAR1
+	if [ $CONFIG1 ]; then
+		VAR1=$CONFIG1
+	else
+		VAR1="-d"
+	fi
+fi
+
+# Time to set how to use curl
+case $VAR1 in
+	"-d")
+		URL=$(curl -F file=@"$VAR2" -- https://0x0.st)
+		;;
+ 	"-#")
+		URL=$(curl -# -F file=@"$VAR2" -- https://0x0.st)
+		;;
+	"-s")
+		URL=$(curl -s -F file=@"$VAR2" -- https://0x0.st)
+		;;
+	"-S")
+		URL=$(curl -s -S -F file=@"$VAR2" -- https://0x0.st)
+		;;
+	*)
+		echo "*** Unsupported option '$VAR1', continue with default setting!"
+		echo
+		URL=$(curl -F file=@"$VAR2" -- https://0x0.st)
+esac
+
+# Transfer file using curl and copy URL returned to clipboard
+echo -n $URL | xclip -i -sel clipboard
+echo
+echo "Tjoho, uploading of '$VAR2' is done! The URL is '$URL'!"
